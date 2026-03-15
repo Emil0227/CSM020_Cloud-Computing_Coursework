f82dbb9c6db3ed0dfa4b661a243087d2f2ba127a
emil0227 <emilcominghome@gmail.com>
2026-03-15 12:32:22 +0800

    adjust on controllers/comments.controller.js

--- MiniWall/src/controllers/comments.controller.js
+++ MiniWall/src/controllers/comments.controller.js
@@ -1,16 +1,18 @@
-console.log('>>> comments.controller.js TOP loaded');
-
 const Comment = require('../models/Comment');
 const Post = require('../models/Post');
 
-console.log('>>> comments.controller models loaded');
-
+/**
+ * Add a comment to a post
+ * Rule: post owner cannot comment on their own post
+ */
 exports.addComment = async (req, res) => {
   const { text } = req.body;
   const { postId } = req.params;
 
   const post = await Post.findById(postId);
-  if (!post) return res.status(404).json({ message: 'Post not found' });
+  if (!post) {
+    return res.status(404).json({ message: 'Post not found' });
+  }
 
   if (post.owner.toString() === req.user.id) {
     return res.status(403).json({ message: 'Post owner cannot comment on own post' });
@@ -22,9 +24,12 @@ exports.addComment = async (req, res) => {
     text,
   });
 
-  res.status(201).json(comment);
+  return res.status(201).json(comment);
 };
 
+/**
+ * Get all comments for a post
+ */
 exports.listComments = async (req, res) => {
   const { postId } = req.params;
 
@@ -32,15 +37,21 @@ exports.listComments = async (req, res) => {
     .populate('owner', 'email displayName')
     .sort({ createdAt: 1 });
 
-  res.json(comments);
+  return res.json(comments);
 };
 
+/**
+ * Update a comment
+ * Rule: only the comment owner can update it
+ */
 exports.updateComment = async (req, res) => {
   const { commentId } = req.params;
   const { text } = req.body;
 
   const comment = await Comment.findById(commentId);
-  if (!comment) return res.status(404).json({ message: 'Comment not found' });
+  if (!comment) {
+    return res.status(404).json({ message: 'Comment not found' });
+  }
 
   if (comment.owner.toString() !== req.user.id) {
     return res.status(403).json({ message: 'Only the comment owner can update' });
@@ -48,22 +59,27 @@ exports.updateComment = async (req, res) => {
 
   comment.text = text;
   await comment.save();
-  res.json(comment);
+
+  return res.json(comment);
 };
 
+/**
+ * Delete a comment
+ * Rule: only the comment owner can delete it
+ */
 exports.deleteComment = async (req, res) => {
   const { commentId } = req.params;
 
   const comment = await Comment.findById(commentId);
-  if (!comment) return res.status(404).json({ message: 'Comment not found' });
+  if (!comment) {
+    return res.status(404).json({ message: 'Comment not found' });
+  }
 
   if (comment.owner.toString() !== req.user.id) {
     return res.status(403).json({ message: 'Only the comment owner can delete' });
   }
 
   await comment.deleteOne();
-  res.json({ message: 'Comment deleted' });
-};
 
-console.log('>>> comments.controller.js BOTTOM loaded');
-console.log('>>> exports keys =', Object.keys(module.exports));
+  return res.json({ message: 'Comment deleted' });
+};
