6591b91d5e8589088aef8a006c64abfe6e6cf9c2
emil0227 <emilcominghome@gmail.com>
2026-03-27 21:07:58 +0800

    add comments and compile scripts src/config/db.js

--- MiniWall/src/config/db.js
+++ MiniWall/src/config/db.js
@@ -1,5 +1,7 @@
+// import Mongoose library to interact with MongoDB
 const mongoose = require('mongoose');
 
+// establish a connection to the MongoDB database
 async function connectDB() {
   const uri = process.env.MONGO_URI;
   if (!uri) throw new Error('MONGO_URI is missing in .env');
@@ -10,4 +12,5 @@ async function connectDB() {
   console.log('MongoDB connected');
 }
 
+// export the function so it can be reused in the main application entry point app.js
 module.exports = connectDB;
