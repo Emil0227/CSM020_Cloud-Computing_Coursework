bfb06efaa5127010bdbea9c4b66cf1e2a4e184db
emil0227 <emilcominghome@gmail.com>
2026-03-29 03:06:25 +0800

    add comments and compile scripts src/middleware/auth.js

--- MiniWall/src/middleware/auth.js
+++ MiniWall/src/middleware/auth.js
@@ -1,5 +1,8 @@
+// import jsonwebtoken library to verify JWT tokens
 const jwt = require('jsonwebtoken');
 
+// middleware to enforce authentication on protected routes
+// validate the JWT token provided in the Authorization header
 function requireAuth(req, res, next) {
   const header = req.headers.authorization || '';
   const [type, token] = header.split(' ');
@@ -21,4 +24,5 @@ function requireAuth(req, res, next) {
   }
 }
 
+// export middleware for use in protected routes
 module.exports = requireAuth;
