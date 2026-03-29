ee3c6530ab62e1f7bdf816ded6c0c1ac9c4ba4a3
emil0227 <emilcominghome@gmail.com>
2026-03-30 01:11:18 +0800

    add comments and compile scripts src/models/Comment.js

--- MiniWall/src/models/Comment.js
+++ MiniWall/src/models/Comment.js
@@ -1,12 +1,15 @@
+// import Mongoose to define schema and interact with MongoDB
 const mongoose = require('mongoose');
 
+// define the schema for Post collection
 const commentSchema = new mongoose.Schema(
   {
     post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
     owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     text: { type: String, required: true, trim: true, maxlength: 500 },
   },
-  { timestamps: true }
+  { timestamps: true } // createdAt, updatedAt (timestamp requirement)
 );
 
+// export the model for use in controllers and services
 module.exports = mongoose.model('Comment', commentSchema);
