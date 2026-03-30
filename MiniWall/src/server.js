230224e419c01b2a645356b090a47a1b028da52e
emil0227 <emilcominghome@gmail.com>
2026-03-31 05:36:01 +0800

    change server.js

--- MiniWall/src/server.js
+++ MiniWall/src/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 (async () => {
   try {
     await connectDB(); // establish connection to MongoDB before starting the server
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`Server running on http://localhost:${PORT}`);
     });
   } catch (err) {
