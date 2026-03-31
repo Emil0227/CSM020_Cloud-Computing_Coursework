4626c551fad175f7a7d5f233429543055d25d68b
emil0227 <emilcominghome@gmail.com>
2026-04-01 05:42:49 +0800

    adjust Post.js to Implement Post Search Functionality

--- MiniWall/src/models/Post.js
+++ MiniWall/src/models/Post.js
@@ -7,12 +7,26 @@ const postSchema = new mongoose.Schema(
     title: { type: String, required: true, trim: true, maxlength: 80 },
     description: { type: String, required: true, trim: true, maxlength: 1000 },
     owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    titleKeywords: {type: [String], index: true},
 
     // denormalised counter for sorting
     likesCount: { type: Number, default: 0, min: 0 },
   },
   { timestamps: true } // createdAt, updatedAt (timestamp requirement)
 );
+
+// pre-save hook to automatically generate titleKeywords
+// ensure keywords are updated whenever a post is created or its title is modified
+postSchema.pre('save', function (next) {
+  if (this.isModified('title')) {
+    this.titleKeywords = this.title
+      .toLowerCase()
+      .split(/\s+/)
+      .filter(Boolean);
+  }
+
+  next();
+});
 
 // export the model for use in controllers and services
 module.exports = mongoose.model('Post', postSchema);
